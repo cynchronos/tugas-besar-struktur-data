976c13b4e6b731f93cb514788653a3c2de0d61d4
Kuro Cybria <rizqikurniawan1863@gmail.com>
2023-06-24 23:11:28 +0700

    Add update stock feature

--- binarysearchtree.py
+++ binarysearchtree.py
@@ -4,6 +4,7 @@ class Node:
         self.left = None
         self.right = None
 
+
 # create bst crud Stok without validation
 class Stock:
     def __init__(self):
@@ -16,40 +17,40 @@ class Stock:
             self._insert(key, self.root)
 
     def _insert(self, key, node):
-        if key['no_sku'] < node.key['no_sku']:
+        if key["no_sku"] < node.key["no_sku"]:
             if node.left is None:
                 node.left = Node(key)
             else:
                 self._insert(key, node.left)
-        elif key['no_sku'] > node.key['no_sku']:
+        elif key["no_sku"] > node.key["no_sku"]:
             if node.right is None:
                 node.right = Node(key)
             else:
                 self._insert(key, node.right)
         else:
-            print('key is already in tree')
+            print("key is already in tree")
 
     def findAll(self):
         if self.root:
             result = []
-            
+
             stack = []
             node = self.root
-            
+
             while node or stack:
                 if node:
-                     stack.append(node)
-                     node = node.left
+                    stack.append(node)
+                    node = node.left
                 else:
                     node = stack.pop()
-                    
+
                     result.append(node.key)
 
                     node = node.right
             return result
         else:
             return None
-    
+
     def findOne(self, query, value, root=None):
         root = self.root if root is None else root
 
@@ -66,51 +67,53 @@ class Stock:
                 return self.findOne(query, value, root.left)
             else:
                 return None
-    
-    # def update_by_sku(self, key, data):
-    #     if self.root is None:
-    #          self.root = Node(key)
-    #     else:
-    #         self._update_by_sku(key, data, self.root)
-    
-    # def _update_by_sku(self, key, data, node):
+
+    def update(self, query, value, data, root=None):
+        root = self.root if root is None else root
+
+        if root:
+            if root.key[query] == value:
+                for index in data.keys():
+                    root.key[index] = data[index]
+                return 'success'
+            elif root.key[query] < value:
+                if root.right is None:
+                    return None
+                return self.update(query, value, root.right)
+            elif root.key[query] > value:
+                if root.left is None:
+                    return None
+                return self.update(query, value, root.left)
+            else:
+                return None
+
+    # def delete(self, key):
+    #     if self.root:
+    #         self.root = self._delete(key, self.root)
+
+    # def _delete(self, key, node):
     #     if node is None:
     #         return node
-    #     if key['no_sku'] < node.key['no_sku']:
-    #         node.left = self._update_by_sku(key, data, node.left)
-    #     elif key['no_sku'] > node.key['no_sku']:
-    #         node.right = self._update_by_sku(key, data, node.right)
-    #     else:
-    #         node.key = data
-    #     return node
+    #     if key["no_sku"] < node.key["no_sku"]:
+    #         node.left = self._delete(key)
 
-    def delete(self, key):
-        if self.root:
-            self.root = self._delete(key, self.root)
 
-    def _delete(self, key, node):
-        if node is None:
-            return node
-        if key['no_sku'] < node.key['no_sku']:
-            node.left = self._delete(key)
-            
 class Transaction:
-    
     data = []
-    
+
     def insert(self, key):
         self.data.append(key)
-        
+
     def insertMany(self, keys):
         for key in keys:
             self.data.append(key)
-    
+
     def findAll(self):
         if len(self.data) != 0:
             return self.data
         else:
             return "Data Kosong"
-    
+
     def findOne(self, query, value):
         if len(self.data) != 0:
             for i in self.data:
@@ -118,7 +121,7 @@ class Transaction:
                     return i
         else:
             return "Data Kosong"
-    
+
     # def update(self, key, data):
     #     if len(self.data) != 0:
     #         self.data[key] = data
--- displaysubtotal.py
+++ displaysubtotal.py
@@ -1,23 +1,31 @@
 from prettytable import PrettyTable
-from binarysearchtree import Transaction
+from binarysearchtree import Stock, Transaction
+
 
 class DisplaySubtotal:
-  
     def sort(self, transaction):
-      # bubble sort in ascending transaction subtotal bst
-      for i in range(len(transaction)):
-        for j in range(len(transaction)-1):
-          if transaction[j]['subtotal'] < transaction[j+1]['subtotal']:
-            transaction[j], transaction[j+1] = transaction[j+1], transaction[j]
-      
-      return transaction
-      
-  
+        # bubble sort in ascending transaction subtotal bst
+        for i in range(len(transaction)):
+            for j in range(len(transaction) - 1):
+                if transaction[j]["subtotal"] < transaction[j + 1]["subtotal"]:
+                    transaction[j], transaction[j + 1] = (
+                        transaction[j + 1],
+                        transaction[j],
+                    )
+
+        return transaction
+
+
 array = [
-  {'nama_konsumen': 'Dodo', 'no_sku': '0001', 'jumlah_beli': 2, 'subtotal': 20000},
-  {'nama_konsumen': 'Mahmud', 'no_sku': '0003', 'jumlah_beli': 3, 'subtotal': 15000},
-  {'nama_konsumen': 'Zeta', 'no_sku': '0002', 'jumlah_beli': 4, 'subtotal': 40000},
-  {'nama_konsumen': 'Arona', 'no_sku': 'SUBA0025', 'jumlah_beli': 3, 'subtotal': 32000}
+    {"nama_konsumen": "Dodo", "no_sku": "0001", "jumlah_beli": 2, "subtotal": 20000},
+    {"nama_konsumen": "Mahmud", "no_sku": "0003", "jumlah_beli": 3, "subtotal": 15000},
+    {"nama_konsumen": "Zeta", "no_sku": "0002", "jumlah_beli": 4, "subtotal": 40000},
+    {
+        "nama_konsumen": "Arona",
+        "no_sku": "SUBA0025",
+        "jumlah_beli": 3,
+        "subtotal": 32000,
+    },
 ]
 
 data = Transaction()
@@ -25,9 +33,36 @@ data.insertMany(array)
 
 transactions = DisplaySubtotal().sort(data.findAll())
 table = PrettyTable()
-table.field_names = ['Nama Konsumen', 'No SKU', 'Jumlah Beli', 'Subtotal']
+table.field_names = ["Nama Konsumen", "No SKU", "Jumlah Beli", "Subtotal"]
 for transaction in transactions:
-    table.add_row([transaction['nama_konsumen'], transaction['no_sku'], transaction['jumlah_beli'], transaction['subtotal']])
+    table.add_row(
+        [
+            transaction["nama_konsumen"],
+            transaction["no_sku"],
+            transaction["jumlah_beli"],
+            transaction["subtotal"],
+        ]
+    )
 
 print(f'Order Sort: {transaction}')
 print(table)
+
+# stock = Stock()
+# stock.insert(
+#     {"no_sku": "0001", "nama_roti": "Roti Tawar", "harga": 10000, "jumlah_stock": 10}
+# )
+
+#! PERLU DIPAHAMI!!
+# sku = input('Masukkan Nomor SKU: ')
+
+# stock_data = stock.findOne("no_sku", sku)
+
+# if stock_data is not None:
+#     print(stock_data)
+#     jumlah_stock = int(input("Masukkan jumlah stock: "))
+#     new_stock = stock_data['jumlah_stock'] + jumlah_stock
+#     result = stock.update('no_sku', stock_data['no_sku'], {'jumlah_stock' : new_stock, 'harga': 15000})
+#     print('Update stok sukses') if result == 'success' else print('Gagal update stock')
+#     print(stock.findOne('no_sku', '0001'))
+# else:
+#     print("Stok tidak ditemukan")
