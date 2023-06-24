1b71b44d37b230f630a8b9c09888b89f628245b6
Kuro Cybria <rizqikurniawan1863@gmail.com>
2023-06-24 20:28:21 +0700

    Update bst Create, Read method and display subtotal

--- binarysearchtree.py
+++ binarysearchtree.py
@@ -4,34 +4,149 @@ class Node:
         self.left = None
         self.right = None
 
-
-class BinarySearchTree:
+# create bst crud Stok without validation
+class Stock:
     def __init__(self):
         self.root = None
 
     def insert(self, key):
-        self.root = self._insert_recursive(self.root, key)
-
-    def _insert_recursive(self, root, key):
-        if root is None:
-            return Node(key)
-        if int(key.sku) < int(root.key.sku):
-            root.left = self._insert_recursive(root.left, key)
+        if self.root is None:
+            self.root = Node(key)
         else:
-            root.right = self._insert_recursive(root.right, key)
-        return root
+            self._insert(key, self.root)
 
-    def search(self, sku):
-        return self._search_recursive(self.root, sku)
+    def _insert(self, key, node):
+        if key['no_sku'] < node.key['no_sku']:
+            if node.left is None:
+                node.left = Node(key)
+            else:
+                self._insert(key, node.left)
+        elif key['no_sku'] > node.key['no_sku']:
+            if node.right is None:
+                node.right = Node(key)
+            else:
+                self._insert(key, node.right)
+        else:
+            print('key is already in tree')
 
-    def _search_recursive(self, root, sku):
-        if root is None or root.key.sku == sku:
-            return root
-        if sku < root.key.sku:
-            return self._search_recursive(root.left, sku)
-        return self._search_recursive(root.right, sku)
+    def findAll(self):
+        if self.root:
+            result = []
+            
+            stack = []
+            node = self.root
+            
+            while node or stack:
+                if node:
+                     stack.append(node)
+                     node = node.left
+                else:
+                    node = stack.pop()
+                    
+                    result.append(node.key)
 
-    def update_stok(self, sku, new_stok):
-        node = self.search(sku)
-        if node:
-            node.key.stok = new_stok
+                    node = node.right
+            return result
+        else:
+            return None
+    
+    def findOne(self, query, value, root=None):
+        root = self.root if root is None else root
+
+        if root:
+            if root.key[query] == value:
+                return root.key
+            elif root.key[query] < value:
+                if root.right is None:
+                    return None
+                return self.findOne(query, value, root.right)
+            elif root.key[query] > value:
+                if root.left is None:
+                    return None
+                return self.findOne(query, value, root.left)
+            else:
+                return None
+        
+    # def inorder(self):
+    #     if self.root:
+    #         self._inorder(self.root)
+
+    # def _inorder(self, node):
+    #     if node:
+    #         self._inorder(node.left)
+    #         print(node.key)
+    #         self._inorder(node.right)
+
+    # def preorder(self):
+    #     if self.root:
+    #         self._preorder(self.root)
+
+    # def _preorder(self, node):
+    #     if node:
+    #         print(node.key)
+    #         self._preorder(node.left)
+    #         self._preorder(node.right)
+
+    # def postorder(self):
+    #     if self.root:
+    #         self._postorder(self.root)
+
+    # def _postorder(self, node):
+    #     if node:
+    #         self._postorder(node.left)
+    #         self._postorder(node.right)
+    #         print(node.key)
+    
+    # def update_by_sku(self, key, data):
+    #     if self.root is None:
+    #          self.root = Node(key)
+    #     else:
+    #         self._update_by_sku(key, data, self.root)
+    
+    # def _update_by_sku(self, key, data, node):
+    #     if node is None:
+    #         return node
+    #     if key['no_sku'] < node.key['no_sku']:
+    #         node.left = self._update_by_sku(key, data, node.left)
+    #     elif key['no_sku'] > node.key['no_sku']:
+    #         node.right = self._update_by_sku(key, data, node.right)
+    #     else:
+    #         node.key = data
+    #     return node
+
+    def delete(self, key):
+        if self.root:
+            self.root = self._delete(key, self.root)
+
+    def _delete(self, key, node):
+        if node is None:
+            return node
+        if key['no_sku'] < node.key['no_sku']:
+            node.left = self._delete(key)
+            
+class Transaction:
+    
+    data = []
+    
+    def insert(self, key):
+        self.data.append(key)
+    
+    def findAll(self):
+        if len(self.data) != 0:
+            return self.data
+        else:
+            return "Data Kosong"
+    
+    def findOne(self, query, value):
+        if len(self.data) != 0:
+            for i in self.data:
+                if i[query] == value:
+                    return i
+        else:
+            return "Data Kosong"
+    
+    # def update(self, key, data):
+    #     if len(self.data) != 0:
+    #         self.data[key] = data
+    #     else:
+    #         return "Data Kosong"
--- displaysubtotal.py
+++ displaysubtotal.py
@@ -1,22 +1,25 @@
-from binarysearchtree import BinarySearchTree
+from binarysearchtree import Stock, Transaction
 
 class DisplaySubtotal:
   
     def sort(self, transaction):
-      # bubble sort in ascending transaction subtotal
+      # bubble sort in ascending transaction subtotal bst
       for i in range(len(transaction)):
-        for j in range(len(transaction)-1-i):
+        for j in range(len(transaction)-1):
           if transaction[j]['subtotal'] < transaction[j+1]['subtotal']:
             transaction[j], transaction[j+1] = transaction[j+1], transaction[j]
+      
       return transaction
+      
     
-data = [
-      {'nama_konsumen': 'Dodo', 'no_sku': '0001', 'jumlah_beli': 2, 'subtotal': 20000},
-      {'nama_konsumen': 'Zeta', 'no_sku': '0002', 'jumlah_beli': 4, 'subtotal': 40000},
-      {'nama_konsumen': 'Mahmud', 'no_sku': '0003', 'jumlah_beli': 3, 'subtotal': 15000},
-      {'nama_konsumen': 'Arona', 'no_sku': '0025', 'jumlah_beli': 3, 'subtotal': 32000},
-    ]
 
-transaction = DisplaySubtotal().sort(data)
+data = Transaction()
+data.insert({'nama_konsumen': 'Dodo', 'no_sku': '0001', 'jumlah_beli': 2, 'subtotal': 20000})
+data.insert({'nama_konsumen': 'Zeta', 'no_sku': '0002', 'jumlah_beli': 4, 'subtotal': 40000})
+data.insert({'nama_konsumen': 'Mahmud', 'no_sku': '0003', 'jumlah_beli': 3, 'subtotal': 15000})
+data.insert({'nama_konsumen': 'Arona', 'no_sku': '0025', 'jumlah_beli': 3, 'subtotal': 32000})
+
+
+transaction = DisplaySubtotal().sort(data.findAll())
 
 print(f'Order Sort: {transaction}')
