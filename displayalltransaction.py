92dce1aa61b626ff4309575fde33a9cb4969f160
Kuro Cybria <rizqikurniawan1863@gmail.com>
2023-06-25 19:44:18 +0700

    edit displayalltransaction.py and restock.py

--- displayalltransaction.py
+++ displayalltransaction.py
@@ -8,25 +8,3 @@ def tampilkan_data_transaksi(data_transaksi):
     for transaksi in data_transaksi:
         table.add_row(transaksi)
     print(table)
-
-# Contoh penggunaan dengan perulangan
-data_transaksi = Transaction()
-data_transaksi.insert(["John Doe", "SKU123", 2, 100])
-data_transaksi.insert(["Jane Smith", "SKU456", 3, 150])
-data_transaksi.insert(["Michael Johnson", "SKU789", 1, 50])
-
-while True:
-    print("Sub Menu Kelola Transaksi Konsumen")
-    print("1. Tampilkan Data Transaksi")
-    print("2. Keluar")
-
-    pilihan = input("Masukkan pilihan (1/2): ")
-
-    if pilihan == "1":
-        tampilkan_data_transaksi(data_transaksi.findAll())
-        input("Tekan Enter untuk melanjutkan...")
-    elif pilihan == "2":
-        break
-    else:
-        print("Pilihan tidak valid. Silakan coba lagi.")
-
--- restock.py
+++ restock.py
@@ -8,13 +8,6 @@ def restok_barang():
         stok_baru = int(input("Masukkan jumlah stok baru: "))
         node['jumlah_stok'] += stok_baru
         Stock.update("no_sku", sku, node)
-        print("Stok barang telah berhasil diperbarui menjadi:", node['jumlah_stok'])
+        return print("Stok barang telah berhasil diperbarui:\n",node['jumlah_stok'])
     else:
-        print("No. SKU tidak ditemukan.")
-
-# Inisialisasi data barang pada BST
-Stock.insert({'no_sku' : '1234', 'nama_barang' : 'Karbu', 'harga_satuan': 150000, 'jumlah_stok' : 10})
-Stock.insert({'no_sku' : '4567', 'nama_barang' : 'Laptop', 'harga_satuan': 2000000, 'jumlah_stok' : 60})
-Stock.insert({'no_sku' : '9999', 'nama_barang' : 'Keyboard', 'harga_satuan': 270000, 'jumlah_stok' : 35})
-# Memanggil fungsi restok_barang
-restok_barang()
+        return print("No. SKU tidak ditemukan.")
