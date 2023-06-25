e93a99a439aca7451a6429e923d73b461f3111c2
azzuhry128 <muhammadazzuhry04@gmail.com>
2023-06-25 19:47:22 +0700

    integrasi dengan bst

--- stockinput.py
+++ stockinput.py
@@ -1,34 +1,41 @@
+from binarysearchtree import Stock
+
+
 class InputStock:
 
-  def __init__(self):
-    self.array = []
+    def __init__(self):
+        self.array = []
 
-  def addNewInput(self, skuNumber, productName, price, stock):
-    is_duplicate = False
-    for sku in self.array:
-      if skuNumber == sku[0]:
-        print("Ditolah, duplikat nomor sku terdeteksi")
-        is_duplicate = True
-        break
+    def addNewInput(self, skuNumber, productName, price, stock):
+        is_duplicate = False
+        for sku in self.array:
+            if skuNumber == sku[0]:
+                print("Ditolak, duplikat nomor sku terdeteksi")
+                is_duplicate = True
+                break
 
-    if is_duplicate == False:
-      inputObject = {skuNumber, productName, price, stock}
-      self.array.append(inputObject)
-      print(self.array)
+        if is_duplicate == False:
+            inputObject = {skuNumber, productName, price, stock}
+            self.array.append(inputObject)
+
+            for items in self.array:
+                Stock.insert(items)
+
+            print(self.array)
 
 
 def addStock():
-  newInput = InputStock()
+    newInput = InputStock()
 
-  while True:
-    inputSku = input("masukkan nomor sku: ")
-    inputProduct = input("masukkan nama produk: ")
-    inputPrice = int(input("masukkan harga produk: "))
-    inputStock = int(input("masukkan jumlah stok: "))
+    while True:
+        inputSku = input("masukkan nomor sku: ")
+        inputProduct = input("masukkan nama produk: ")
+        inputPrice = int(input("masukkan harga produk: "))
+        inputStock = int(input("masukkan jumlah stok: "))
 
-    newInput.addNewInput(inputSku, inputProduct, inputPrice, inputStock)
+        newInput.addNewInput(inputSku, inputProduct, inputPrice, inputStock)
 
-    choice = input("input another stuff ?   [Y/N]")
+        choice = input("input another stuff ?   [Y/N]")
 
-    if choice != "Y" and choice != "y":
-      break
+        if choice != "Y" and choice != "y":
+            break
