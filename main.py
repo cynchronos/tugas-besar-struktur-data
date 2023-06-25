f4a7869e836b4ebf4fdd4db2ffb75d4954177587
Kuro Cybria <rizqikurniawan1863@gmail.com>
2023-06-26 02:15:45 +0700

    Move seed data from main.py to seeddata.py

--- main.py
+++ main.py
@@ -1,29 +1,9 @@
 import os
-from binarysearchtree import Transaction
 from transaction import tambah_data_transaksi
 from displaysubtotal import sort
 from stockinput import addStock
-from binarysearchtree import Stock
 from displayalltransaction import tampilkan_data_transaksi
 from restock import restok_barang
-
-Stock.insert({'nama_barang': 'Susu Ganyu', 'no_sku': 2213, 'harga_satuan': 10000, 'jumlah_stok': 10})
-Stock.insert({'nama_barang': 'Pelet Kokomi', 'no_sku': 2231, 'harga_satuan': 4000, 'jumlah_stok': 10})
-Stock.insert({'nama_barang': 'Pedang Tumpul', 'no_sku': 4421, 'harga_satuan': 25000, 'jumlah_stok': 10})
-
-array = [
-    {"nama_konsumen": "Dodo", "no_sku": "0001", "jumlah_beli": 2, "subtotal": 20000},
-    {"nama_konsumen": "Mahmud", "no_sku": "0003", "jumlah_beli": 3, "subtotal": 15000},
-    {"nama_konsumen": "Zeta", "no_sku": "0002", "jumlah_beli": 4, "subtotal": 40000},
-    {
-        "nama_konsumen": "Arona",
-        "no_sku": "SUBA0025",
-        "jumlah_beli": 3,
-        "subtotal": 32000,
-    },
-]
-
-Transaction.insertMany(array)
 
 def main():
     os.system("cls")
